706fbdd5255afc2f6761218de2ff4208644357af
Sean Harper <seharper@microsoft.com>
2022-04-26 11:54:28 -0400

    Help contents added and updated

--- BPCustomRules/BPCustomRules.psm1
+++ BPCustomRules/BPCustomRules.psm1
@@ -8,12 +8,14 @@ if ([System.Threading.Thread]::CurrentThread.CurrentUICulture.Name -ne 'en-US') 
 else {
     Import-LocalizedData -BindingVariable Messages
 }
+
+#region Unnecessary Comments
 <#
 .SYNOPSIS
     Removes these unnecessary comments.
 .DESCRIPTION
     Don't precede each line of code with a comment. Doing so breaks up the code and makes it harder to follow. A well-written PowerShell command, with full command and parameter names, can be pretty self-explanatory.
-    Don't comment-explain it unless it isn't self-explanatory.To fix a violation of this rule, please remove these unnecessary comments.
+    Don't comment-explain it unless it isn't self-explanatory. To fix a violation of this rule, please remove these unnecessary comments.
 .EXAMPLE
     Measure-OverComment -Token $Token
 .INPUTS
@@ -66,23 +68,25 @@ function Measure-OverComment {
         }
     }
 }
+#endregion Unnecessary Comments
 
+#region Advanced Functions --INCOMPLETE
 <#
 .SYNOPSIS
-    Adds a .NOTE keyword in your comment-based help.
+    Finds and detects advanced function capability
 .DESCRIPTION
-    Comment-based help is written as a series of comments. You can write comment-based help topics for end users to better understand your functions. Additionally, it’s better to explain the detail about how the function works.
-    To fix a violation of this rule, add a .NOTE keyword in your comment-based help. You can get more details by running “Get-Help about_Comment_Based_Help” command in Windows PowerShell.
+    Advanced functions add additional capabilities that are not present in simple functions.  
+    Capabilities to your scripts including commands such as whatif, verbose, Debug.
 .EXAMPLE
-    Measure-HelpNote -FunctionDefinitionAst $FunctionDefinitionAst
+    Measure-AdvancedFunction -FunctionDefinitionAst $FunctionDefinitionAst
 .INPUTS
     [System.Management.Automation.Language.FunctionDefinitionAst]
 .OUTPUTS
     [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
 .NOTES
-    Reference: Writing Help and Comments, Windows PowerShell Best Practices.
+    Reference: 6.4 UseAdvancedFunctionsAttribute (Custom)
 #>
-function Measure-HelpNote {
+function Measure-AdvancedFunction {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
     Param
@@ -118,13 +122,9 @@ function Measure-HelpNote {
             # Return directly if function is not an advanced function.
             [System.Management.Automation.Language.AttributeAst[]]$attrAsts = $FunctionDefinitionAst.Find($predicate, $true)
             if ($FunctionDefinitionAst.IsWorkflow -or !$attrAsts) {
-                return $results
-            }
-
-            if (!$FunctionDefinitionAst.GetHelpContent().Notes) {
                 $result = New-Object `
                     -TypeName "Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord" `
-                    -ArgumentList $Messages.MeasureHelpNote, $FunctionDefinitionAst.Extent, $PSCmdlet.MyInvocation.InvocationName, Warning, $null
+                    -ArgumentList $Messages.MeasureAdvancedFunctions, $FunctionDefinitionAst.Extent, $PSCmdlet.MyInvocation.InvocationName, Information, $null
 
                 $results += $result
             }
@@ -136,39 +136,97 @@ function Measure-HelpNote {
         }
     }
 }
+#endregion Advanced Functions
 
+#region PSNoKeys  --INCOMPLETE--
+<#
+.SYNOPSIS
+    Checks whether a string could potentially be a Key, Certificate or password.
+.DESCRIPTION
+    MAPS code should never contain keys in plain text under any circumstances
+    Keys can end up in commit history or exposed to screen-scraper malware
+.EXAMPLE
+    Measure-LinesEndingWithSemicolons -Token $Token
+.INPUTS
+    [System.Management.Automation.Language.Token[]]
+.OUTPUTS
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+.NOTES
+    Reference: 8.5 PSNoKeys (Custom)
+#>
 function Measure-PotentialPasswordsOrKeys {
 
 }
+#endregion PSNoKeys
 
+#region Function Size by Lines
+<#
+.SYNOPSIS
+    Function size should be limited to a maximum of 150 lines
+.DESCRIPTION
+    It is best practice for functions to do one thing. This will enforce modular, testable, and more efficient code
+.EXAMPLE
+    Measure-FunctionSizeByLines -FunctionDefinitionAst $FunctionDefinitionAst
+.INPUTS
+    [System.Management.Automation.Language.FunctionDefinitionAst]
+.OUTPUTS
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+.NOTES
+    Reference: 6.3 PSFunctionSizeLimit (Custom)
+#>
 function Measure-FunctionSizeByLines {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
     Param
     (
-        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinitionAst
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinitionAst
     )
 
     Process {
         $results = @()
         
-        if (($FunctionDefinitionAst.Extent.EndScriptPosition.LineNumber - $FunctionDefinitionAst.Extent.StartScriptPosition.LineNumber) -gt 10) {
-            $result = New-Object `
-                -TypeName "Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord" `
-                -ArgumentList $Messages.MeasureFunctionSizeByLines, $FunctionDefinitionAst.Extent, $PSCmdlet.MyInvocation.InvocationName, Warning, $null
+        try {
+            if (($FunctionDefinitionAst.Extent.EndScriptPosition.LineNumber - $FunctionDefinitionAst.Extent.StartScriptPosition.LineNumber) -gt 10) {
+                $result = New-Object `
+                    -TypeName "Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord" `
+                    -ArgumentList $Messages.MeasureFunctionSizeByLines, $FunctionDefinitionAst.Extent, $PSCmdlet.MyInvocation.InvocationName, Warning, $null
         
-            $results += $result
+                $results += $result
+            }
+            return $results
         }
-        return $results
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
     }
 }
+#endregion
 
+#region Lines by Character Count
+<#
+.SYNOPSIS
+    Max Line length should be 120 characters
+.DESCRIPTION
+    Keeping lines to a small width allows scripts to be read in one direction (top to bottom) without scrolling back-and-forth horizontally
+    Debugging and reading the code is a lot easier
+    This is particularly valuable for reading Runbook code in the Azure Automation Account
+.EXAMPLE
+    Measure-LinesByCharacterCount -Token $Token
+.INPUTS
+    [System.Management.Automation.Language.CommandAst]
+.OUTPUTS
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+.NOTES
+    Reference: 3.3 AvoidLongLines (Default)
+#>
 function Measure-LinesByCharacterCount {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
     Param
     (
-        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][System.Management.Automation.Language.CommandAst]$CommandAst
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.CommandAst]$CommandAst
     )
 
     Process {
@@ -188,7 +246,9 @@ function Measure-LinesByCharacterCount {
         }
     }
 }
-#>
+#endregion Lines by Character Count
+
+#region Lines Ending with a Semicolon
 <#
 .SYNOPSIS
     Searches for lines ending in a semicolon
@@ -232,7 +292,24 @@ function Measure-LinesEndingWithSemicolons {
         }
     }
 }
+#endregion Lines Ending with a Semicolon
 
+#region FunctionsPascalCasing  --INCOMPLETE--
+<#
+.SYNOPSIS
+    Function names should follow PowerShell's Verb-Noun naming conventions.
+.DESCRIPTION
+    PowerShell uses PascalCase for all public identifiers including classes and enums. 
+    Function names should follow PowerShell's Verb-Noun naming conventions.
+.EXAMPLE
+    Measure-PascalCaseFunctionNames -FunctionDefinitionAst $FunctionDefinitionAst
+.INPUTS
+    [System.Management.Automation.Language.FunctionDefinitionAst]
+.OUTPUTS
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+.NOTES
+    Reference: 5.5 FunctionsPascalCasing (Custom)
+#>
 function Measure-PascalCaseFunctionNames {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
@@ -244,11 +321,43 @@ function Measure-PascalCaseFunctionNames {
 
     Process {
         $results = @()
+        try {
+            [ScriptBlock]$predicate = {
+                param ([System.Management.Automation.Language.Ast]$Ast)
+                [bool]$returnValue = $false
+
+                if ($Ast -is [System.Management.Automation.Language.AttributeAst]) {
+                    [System.Management.Automation.Language.AttributeAst]$attrAst = $ast;
+                    if ($attrAst.TypeName.Name -eq 'CmdletBinding') {
+                        $returnValue = $true
+                    }
+                }
+
+                return $returnValue
+            }
+        }
+        catch {}
         
         return $results
     }
 }
+#endregion FunctionsPascalCasing
 
+#region VariableCamelCasing
+<#
+.SYNOPSIS
+    Variables should follow camel casing
+.DESCRIPTION
+    Variables should follow camel casing
+.EXAMPLE
+    Measure-CamelCaseVariableNames -Token $Token
+.INPUTS
+    [System.Management.Automation.Language.Token[]]
+.OUTPUTS
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+.NOTES
+    Reference: 3.7 No Semicolons as Line Terminators (Custom)
+#>
 function Measure-CamelCaseVariableNames {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
@@ -267,7 +376,7 @@ function Measure-CamelCaseVariableNames {
         $results = @()
         
         foreach ($subToken in $Token) {
-            if (($subToken -notin $autoVariables) -and ($subToken -cmatch '\$[A-Z]')) {
+            if (($subToken -is [System.Management.Automation.Language.VariableToken]) -and ($subToken.Name -cnotin $autoVariables) -and ($subToken.Name -cmatch '^[A-Z]')) {
                 $result = New-Object `
                     -TypeName "Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord" `
                     -ArgumentList $Messages.MeasureCamelCaseVariableNames, $subToken.Extent, $PSCmdlet.MyInvocation.InvocationName, Warning, $null
@@ -278,13 +387,30 @@ function Measure-CamelCaseVariableNames {
         return $results
     }
 }
+#endregion VariableCamelCasing
 
+#region PSFunctionNaming  --INCOMPLETE--
+<#
+.SYNOPSIS
+    Functions should follow PowerShell’s verb-noun naming convention
+.DESCRIPTION
+    Functions should follow PowerShell’s verb-noun naming convention
+.EXAMPLE
+    Measure-VerbNounFunctionNames -FunctionDefinitionAst $FunctionDefinitionAst
+.INPUTS
+    [System.Management.Automation.Language.FunctionDefinitionAst]
+.OUTPUTS
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+.NOTES
+    Reference: 6.2 PSFunctionNaming (Custom)
+#>
 function Measure-VerbNounFunctionNames {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
     Param
     (
-        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinitionAst
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinitionAst
     )
 
     Process {
@@ -293,7 +419,23 @@ function Measure-VerbNounFunctionNames {
         return $results
     }
 }
+#endregion PSFunctionNaming
 
+#region FunctionsShouldBeReferenced --INCOMPLETE--
+<#
+.SYNOPSIS
+    All functions should be referenced
+.DESCRIPTION
+    Functions that are created but are not assigned create technical debt
+.EXAMPLE
+    Measure-OrphanedFunctions -FunctionDefinitionAst $FunctionDefinitionAst
+.INPUTS
+    [System.Management.Automation.Language.FunctionDefinitionAst]
+.OUTPUTS
+    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+.NOTES
+    Reference: 6.5 FunctionsShouldBeReferenced (Custom)
+#>
 function Measure-OrphanedFunctions {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
@@ -308,6 +450,7 @@ function Measure-OrphanedFunctions {
         return $results
     }
 }
+#endregion FunctionsShouldBeReferenced
 
 Export-ModuleMember -Function Measure*
 
